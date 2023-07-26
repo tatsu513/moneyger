580df889bc08e3ef9a6129300fcd9cc8e650f0b6
tatsu513 <0285mashiko@gmail.com>
2023-07-27 00:18:13 +0900

    back code a little

--- src/app/payments/_main/ListPaymentWithSuspense.tsx
+++ src/app/payments/_main/ListPaymentWithSuspense.tsx
@@ -1,32 +1,36 @@
 'use client';
-import { Divider, List } from '@mui/material';
+import { graphql } from '@/dao/generated/preset';
+import { useMemo } from 'react';
+import getUrqlVariables from '@/util/getUrqlVariables';
+import { useQuery } from 'urql';
+import { Divider, List, Typography } from '@mui/material';
 
-// const listPaymentWithSuspenseDocument = graphql(`
-//   query listPayments {
-//     listPayments {
-//       id
-//       name
-//       maxAmount
-//       currentAmount
-//     }
-//   }
-// `);
+const listPaymentWithSuspenseDocument = graphql(`
+  query listPayments {
+    listPayments {
+      id
+      name
+      maxAmount
+      currentAmount
+    }
+  }
+`);
 
 const ListPaymentWithSuspense: React.FC = () => {
-  // const val = useMemo(() => {
-  //   return getUrqlVariables(listPaymentWithSuspenseDocument, {}, true);
-  // }, []);
-  // const [{ data }] = useQuery(val);
+  const val = useMemo(() => {
+    return getUrqlVariables(listPaymentWithSuspenseDocument, {}, true);
+  }, []);
+  const [{ data }] = useQuery(val);
 
-  // if (data == null) {
-  //   console.info('収支項目を取得できませんでした');
-  //   // throw new Error('収支項目を取得できませんでした');
-  // }
+  if (data == null) {
+    console.info('収支項目を取得できませんでした');
+    // throw new Error('収支項目を取得できませんでした');
+  }
 
-  // const listPayments = data?.listPayments ?? [];
-  // if (listPayments.length === 0) {
-  //   return <Typography>データがありません</Typography>;
-  // }
+  const listPayments = data?.listPayments ?? [];
+  if (listPayments.length === 0) {
+    return <Typography>データがありません</Typography>;
+  }
   return (
     <List>
       <Divider component="li" />
