a1ca798c44cfd564373161e9484415b4ad079c3a
tatsu513 <0285mashiko@gmail.com>
2023-07-27 00:22:08 +0900

    use Typography only

--- src/app/payments/_main/ListPaymentWithSuspense.tsx
+++ src/app/payments/_main/ListPaymentWithSuspense.tsx
@@ -3,7 +3,7 @@ import { graphql } from '@/dao/generated/preset';
 import { useMemo } from 'react';
 import getUrqlVariables from '@/util/getUrqlVariables';
 import { useQuery } from 'urql';
-import { Divider, List, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 
 const listPaymentWithSuspenseDocument = graphql(`
   query listPayments {
@@ -31,21 +31,7 @@ const ListPaymentWithSuspense: React.FC = () => {
   if (listPayments.length === 0) {
     return <Typography>データがありません</Typography>;
   }
-  return (
-    <List>
-      <Divider component="li" />
-      <>大川達也です</>
-      {/* {data?.listPayments.map((p) => (
-        <PaymentListItem
-          key={p.name}
-          id={p.id}
-          name={p.name}
-          currentAmount={p.currentAmount}
-          maxAmount={p.maxAmount}
-        />
-      ))} */}
-    </List>
-  );
+  return <Typography>大川達也です</Typography>;
 };
 
 export default ListPaymentWithSuspense;
