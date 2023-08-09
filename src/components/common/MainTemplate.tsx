ab3ca48e94cbf7e0ee5ead0834afef1f51f05d83
tatsu513 <0285mashiko@gmail.com>
2023-08-10 00:06:01 +0900

    update template

--- src/components/common/MainTemplate.tsx
+++ src/components/common/MainTemplate.tsx
@@ -7,12 +7,24 @@ import MoneygerBottomNavigation from '@/components/common/MoneygerBottomNavigati
 
 const MainTemplate: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <Box display="flex" flexDirection="column" height="100vh">
+    <Box
+      display="flex"
+      flexDirection="column"
+      height="100dvh"
+      pt="env(safe-area-inset-top)"
+      pr="env(safe-area-inset-right)"
+      pb="env(safe-area-inset-bottom)"
+      pl="env(safe-area-inset-left)"
+    >
       <MoneygerAppBar />
-      <Box flex="auto" sx={{ overflowX: 'scroll' }}>
-        <PageContentsTemplate>{children}</PageContentsTemplate>
+      <Box display="flex" flexDirection="column" height="calc(100% - 56px)">
+        <Box flex={1} py={2} sx={{ overflowY: 'scroll' }}>
+          <PageContentsTemplate>{children}</PageContentsTemplate>
+        </Box>
+        <Box>
+          <MoneygerBottomNavigation />
+        </Box>
       </Box>
-      <MoneygerBottomNavigation />
     </Box>
   );
 };
