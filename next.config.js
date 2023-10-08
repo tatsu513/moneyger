1244ab2f8231566627fc529af695abcfd61021df
大川達也 <tatsuya.okawa@1-2-283.local>
2023-10-08 13:25:55 +0900

    update lock

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-const withTM = require('next-transpile-modules')(['string-width', 'strip-ansi', 'build']);
-
 /* eslint-disable */
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = withTM(nextConfig)
+module.exports = nextConfig
